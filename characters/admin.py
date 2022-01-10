d01d8d7cd3d633fafb707ae5ed8aa78becb0b673
Deibys <atom32@tutanota.com>
2022-01-10 16:22:57 -0400

    admin for Character model added

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from characters.models import Character
+
 # Register your models here.
+
+class CharacterAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Character
+    '''
+    list_display = ('name','alter_ego','power')
+    list_filter = ('power',)
+    search_fields = ('name','alter_ego')
+
+admin.site.register(Character, CharacterAdmin)
